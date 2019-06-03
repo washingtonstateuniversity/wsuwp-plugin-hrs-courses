fae7a1f3c0eed3adfa86e4c6be3ff7a9134f519c
Adam Turner <adam.turner@wsu.edu>
2019-06-03 10:40:32 -0700

    :sparkles: Add function to modify the WP nav menu classes
    We don't want the standard blog page to be identified in the nav
    menu as the parent of the courses CPT posts. This function filters
    the nav menu item classes array to remove the "active" class from
    the blog home page when viewing any Courses CPT page or post, and
    reassigns it to the 'post_type_archive' nav menu item.

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -31,3 +31,40 @@ function load_template( $template ) {
 }
 add_filter( 'taxonomy_template', __NAMESPACE__ . '\load_template' );
 add_filter( 'archive_template', __NAMESPACE__ . '\load_template' );
+
+/**
+ * Modifies the WP nav menu classes.
+ *
+ * Hooked using a later priority to allow parent theme to finish its own
+ * modifications first.
+ *
+ * @since 0.4.0
+ *
+ * @param array    $classes Required. Current list of nav menu item classes.
+ * @param WP_Post  $item    Post object representing the menu item data.
+ * @param stdClass $args    The arguments used to create the menu.
+ * @return string[] Array of CSS classes for the nav menu item.
+ */
+function modify_nav_menu_classes( $classes, $item, $args ) {
+	if ( in_array( $args->menu, array( 'site', 'offsite' ) ) ) {
+		// Remove the "active" class from the blog home page for Courses CPT.
+		if (
+			is_singular( Setup\WSUWP_HRS_Courses::$post_type_slug ) ||
+			is_post_type_archive( Setup\WSUWP_HRS_Courses::$post_type_slug ) ||
+			is_tax( 'course_tag' ) ||
+			is_tax( 'learning_program' )
+		) {
+			if ( 'News' === $item->title ) {
+				$classes = array();
+			}
+		}
+
+		// Assign the "active" class to the proper menu item parent for singular course.
+		if ( is_singular( Setup\WSUWP_HRS_Courses::$post_type_slug ) && 'post_type_archive' === $item->type ) {
+			$classes[] = 'active';
+		}
+	}
+
+	return $classes;
+}
+add_filter( 'nav_menu_css_class', __NAMESPACE__ . '\modify_nav_menu_classes', 15, 3 );

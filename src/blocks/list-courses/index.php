1b3e1924c3688dbc3a3fb22f488881af8eabe9f2
Adam Turner <adam.turner@wsu.edu>
2019-08-23 09:05:04 -0700

    Display a "no courses found" message when query returns empty

--- src/blocks/list-courses/index.php
+++ src/blocks/list-courses/index.php
@@ -37,13 +37,20 @@ function render_block_list_courses( $attributes ) {
 		);
 	}
 
-	var_dump( $attributes ); // DEBUG
-
 	$recent_courses = get_posts( $args );
 
-	$list_items_markup = '';
+	if ( ! $recent_courses ) {
+		$block_content = sprintf(
+			'<div class="components-placeholder"><div class="components-placeholder__label">%1$s %2$s</div><div class="components-placeholder__fieldset">%3$s</div></div>',
+			'<svg aria-hidden="true" role="img" focusable="false" class="dashicon dashicons-admin-post" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M10.44 3.02l1.82-1.82 6.36 6.35-1.83 1.82c-1.05-.68-2.48-.57-3.41.36l-.75.75c-.92.93-1.04 2.35-.35 3.41l-1.83 1.82-2.41-2.41-2.8 2.79c-.42.42-3.38 2.71-3.8 2.29s1.86-3.39 2.28-3.81l2.79-2.79L4.1 9.36l1.83-1.82c1.05.69 2.48.57 3.4-.36l.75-.75c.93-.92 1.05-2.35.36-3.41z"></path></svg>',
+			__( 'List Courses' ),
+			__( 'No courses found.' )
+		);
+		return $block_content;
+	}
 
-	$excerpt_length = $attributes['excerptLength'];
+	$list_items_markup = '';
+	$excerpt_length    = $attributes['excerptLength'];
 
 	foreach ( $recent_courses as $course ) {
 		$title = get_the_title( $course );

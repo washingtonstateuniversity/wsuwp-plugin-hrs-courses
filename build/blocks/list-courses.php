8924562a2845b6aa27e90f0107cea2c3ffaca455
Adam Turner <adam.turner@wsu.edu>
2019-08-23 09:17:19 -0700

    :package: Update compiled files

--- build/blocks/list-courses.php
+++ build/blocks/list-courses.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Server-side rendering of the `hrscourses/latest-courses` block.
+ * Server-side rendering of the `hrscourses/list-courses` block.
  *
  * @package WSUWP_HRS_Courses
  * @since 0.4.0
  */
-namespace WSUWP\HRS\Courses\Blocks\latest_courses;
+namespace WSUWP\HRS\Courses\Blocks\list_courses;
 use WSUWP\HRS\Courses\Setup;
 
 /**
- * Renders the `hrscourses/latest-courses` dynamic block contents.
+ * Renders the `hrscourses/list-courses` dynamic block contents.
  *
  * @since 0.4.0
  *
@@ -17,7 +17,7 @@ use WSUWP\HRS\Courses\Setup;
  *
  * @return string The formatted HTML for display.
  */
-function render_block_latest_courses( $attributes ) {
+function render_block_list_courses( $attributes ) {
 	$args = array(
 		'posts_per_page'   => $attributes['coursesToShow'],
 		'post_type'        => Setup\WSUWP_HRS_Courses::$post_type_slug,
@@ -31,7 +31,7 @@ function render_block_latest_courses( $attributes ) {
 		$args['tax_query'] = array(
 			array(
 				'taxonomy' => 'learning_program',
-				'field'    => 'slug',
+				'field'    => 'id',
 				'terms'    => $attributes['learningPrograms'],
 			),
 		);
@@ -39,9 +39,18 @@ function render_block_latest_courses( $attributes ) {
 
 	$recent_courses = get_posts( $args );
 
-	$list_items_markup = '';
+	if ( ! $recent_courses ) {
+		$block_content = sprintf(
+			'<div class="components-placeholder"><div class="components-placeholder__label">%1$s %2$s</div><div class="components-placeholder__fieldset">%3$s</div></div>',
+			'<svg aria-hidden="true" role="img" focusable="false" class="dashicon dashicons-admin-post" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M10.44 3.02l1.82-1.82 6.36 6.35-1.83 1.82c-1.05-.68-2.48-.57-3.41.36l-.75.75c-.92.93-1.04 2.35-.35 3.41l-1.83 1.82-2.41-2.41-2.8 2.79c-.42.42-3.38 2.71-3.8 2.29s1.86-3.39 2.28-3.81l2.79-2.79L4.1 9.36l1.83-1.82c1.05.69 2.48.57 3.4-.36l.75-.75c.93-.92 1.05-2.35.36-3.41z"></path></svg>',
+			__( 'List Courses' ),
+			__( 'No courses found.' )
+		);
+		return $block_content;
+	}
 
-	$excerpt_length = $attributes['excerptLength'];
+	$list_items_markup = '';
+	$excerpt_length    = $attributes['excerptLength'];
 
 	foreach ( $recent_courses as $course ) {
 		$title = get_the_title( $course );
@@ -56,7 +65,7 @@ function render_block_latest_courses( $attributes ) {
 
 		if ( isset( $attributes['displayPostDate'] ) && $attributes['displayPostDate'] ) {
 			$list_items_markup .= sprintf(
-				'<time datetime="%1$s" class="wp-block-hrscourses-latest-courses__post-date">%2$s</time>',
+				'<time datetime="%1$s" class="wp-block-hrscourses-list-courses__post-date">%2$s</time>',
 				esc_attr( get_the_date( 'c', $course ) ),
 				esc_html( get_the_date( '', $course ) )
 			);
@@ -71,7 +80,7 @@ function render_block_latest_courses( $attributes ) {
 			$trimmed_excerpt = esc_html( wp_trim_words( $course_excerpt, $excerpt_length, ' &hellip; ' ) );
 
 			$list_items_markup .= sprintf(
-				'<div class="wp-block-hrscourses-latest-courses__post-excerpt">%1$s',
+				'<div class="wp-block-hrscourses-list-courses__post-excerpt">%1$s',
 				$trimmed_excerpt
 			);
 
@@ -91,7 +100,7 @@ function render_block_latest_courses( $attributes ) {
 		if ( isset( $attributes['displayCourseContent'] ) && $attributes['displayCourseContent']
 			&& isset( $attributes['displayCourseContentRadio'] ) && 'full_post' === $attributes['displayCourseContentRadio'] ) {
 			$list_items_markup .= sprintf(
-				'<div class=".wp-block-hrscourses-latest-courses__full_content">%1$s</div>',
+				'<div class=".wp-block-hrscourses-list-courses__full_content">%1$s</div>',
 				wp_kses_post( html_entity_decode( $course->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) )
 			);
 		}
@@ -99,7 +108,7 @@ function render_block_latest_courses( $attributes ) {
 		$list_items_markup .= "</li>\n";
 	}
 
-	$class = 'wp-block-hrscourses-latest-courses wp-block-latest-posts__list';
+	$class = 'wp-block-hrscourses-list-courses wp-block-latest-posts__list';
 	if ( isset( $attributes['align'] ) ) {
 		$class .= ' align' . $attributes['align'];
 	}
@@ -130,13 +139,13 @@ function render_block_latest_courses( $attributes ) {
 }
 
 /**
- * Registers the `hrscourses/latest-courses` on the server.
+ * Registers the `hrscourses/list-courses` on the server.
  *
  * @since 0.4.0
  */
-function register_block_latest_courses() {
+function register_block_list_courses() {
 	register_block_type(
-		'hrscourses/latest-courses',
+		'hrscourses/list-courses',
 		array(
 			'attributes'      => array(
 				'align'                     => array(
@@ -186,9 +195,9 @@ function register_block_latest_courses() {
 					'default' => 'date',
 				),
 			),
-			'render_callback' => __NAMESPACE__ . '\render_block_latest_courses',
+			'render_callback' => __NAMESPACE__ . '\render_block_list_courses',
 		)
 	);
 }
 // Use later priority to make sure required resources are ready.
-add_action( 'init', __NAMESPACE__ . '\register_block_latest_courses', 25 );
+add_action( 'init', __NAMESPACE__ . '\register_block_list_courses', 25 );

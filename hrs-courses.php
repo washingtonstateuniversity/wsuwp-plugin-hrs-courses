cf9bba805fade77e1a1da2e11384c793bf4f0b09
Adam Turner <act@adamturner.dev>
2022-11-10 11:59:48 -0800

    Bump WP tested-to to 6.1.1

--- hrs-courses.php
+++ hrs-courses.php
@@ -9,7 +9,7 @@
  * Update URI: https://api.github.com/repos/washingtonstateuniversity/wsuwp-plugin-hrs-courses/releases/latest
  * Text Domain: wsuwp-hrs-courses
  * Requires at least: 5.7
- * Tested up to: 6.0.0
+ * Tested up to: 6.1.1
  * Requires PHP: 7.0
  *
  * @package WSUWP_HRS_Courses
